14ebe6000928b1f7f629d300f950675dc2c928d8
HyoungSunChoi <68225321+HyoungSunChoi@users.noreply.github.com>
2021-04-22 16:44:07 +0900

    Delete 정렬1번.py

--- 프로그래머스 코딩테스트/정렬1번.py
+++
@@ -1,8 +0,0 @@
-def solution(num):
-    str_list = [int((str(n) * 5)[:5]) for n in num]
-    result = ''
-    for i in range(len(str_list)):
-        idx = str_list.index(max(str_list))
-        result += str(num[idx])
-        str_list[idx], num[idx] = 0, 0
-    return str(int(result))
